8f4d408793b337cbc8e500120a4478e2a5f057b2
Артем <forsakl123@gmail.com>
2024-01-16 15:06:27 +0500

    tasks done

--- src/main/java/skypro/Homework17/calculator/controller/CalculatorController.java
+++ src/main/java/skypro/Homework17/calculator/controller/CalculatorController.java
@@ -1,4 +1,52 @@
 package skypro.Homework17.calculator.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import skypro.Homework17.calculator.service.CalculatorService;
+
+@RestController
+@RequestMapping("/calculator")
 public class CalculatorController {
+    private final CalculatorService calculatorService;
+
+    public CalculatorController(CalculatorService calculatorService){
+        this.calculatorService = calculatorService;
+    }
+    @GetMapping
+    public String showHelloWorld(){return "Добро пожаловать в калькулятор";}
+
+    @GetMapping("/plus")
+    public String showPlus(@RequestParam(required = false) Integer num1
+            ,@RequestParam(required = false) Integer num2) {
+        if (num1==null||num2==null){return "Укажите параметры num1 и num2";}
+        return num1 +"+"+num2+"="+calculatorService.getPlus(num1,num2);
+    }
+
+    @GetMapping("/minus")
+    public String showMinus(@RequestParam(required = false) Integer num1
+            ,@RequestParam(required = false) Integer num2) {
+        if (num1==null||num2==null){return "Укажите параметры num1 и num2";}
+        return num1 +"-"+num2+"="+calculatorService.getMinus(num1,num2);
+    }
+
+    @GetMapping("/multiply")
+    public String showMultiply(@RequestParam(required = false) Integer num1
+            ,@RequestParam(required = false) Integer num2) {
+        if (num1==null||num2==null){return "Укажите параметры num1 и num2";}
+        return num1 +"*"+num2+"="+calculatorService.getMultiply(num1,num2);
+    }
+
+    @GetMapping("/divide")
+    public String showDivide(@RequestParam(required = false) Integer num1
+            ,@RequestParam(required = false) Integer num2)  {
+        if (num1==null||num2==null){
+            return "Укажите параметры num1 и num2";
+        }
+        if (num2==0) {
+            return "Деление на 0";
+        }
+        return num1 +"/"+num2+"="+calculatorService.getDivide(num1,num2);
+    }
 }
--- src/main/java/skypro/Homework17/calculator/service/CalculatorService.java
+++ src/main/java/skypro/Homework17/calculator/service/CalculatorService.java
@@ -1,4 +1,8 @@
 package skypro.Homework17.calculator.service;
 
 public interface CalculatorService {
+    int getPlus(int num1,int num2);
+    int getMinus(int num1,int num2);
+    int getMultiply(int num1,int num2);
+    int getDivide(int num1,int num2);
 }
